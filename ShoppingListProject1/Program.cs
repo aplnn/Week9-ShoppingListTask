e13e2c64b108b05e3c938328e3bd0e2b9859b12d
Anni Plnn <annipalanen@gmail.com>
2022-04-03 18:36:12 +0300

    add new projects

---
+++ ShoppingListProject1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace ShoppingListProject1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Soovid luua omale poenimekirja? Y/N");
+            char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+            if (userInput == 'y')
+            {
+                string rootDirectory = @"C:\Users\...\samples";
+                string newDirectory = "shoppingList";
+                string fileName = "myshoppingList.txt";
+
+                Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{ newDirectory}\\{fileName}");
+
+                Console.WriteLine("Sinu fail on loodud!");
+            }
+            else
+            {
+                Console.WriteLine("Pole viga, vahest järgmine kord!");
+            }
+        }
+    }
+}
---
+++ ShoppingListProject2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ShoppingListProject2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = @"C:\Users\...\samples\shoppingList";
+            string fileNamed = @"\\myshoppingList.txt";
+
+            string[] arrayFromFile = File.ReadAllLines($"{fileLocation}{fileNamed}");
+            List<string> myshoppingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Soovid lisada eseme ostunimekirja? Y/N");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Sisesta ese:");
+                    string userWish = Console.ReadLine();
+                    myshoppingList.Add(userWish);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Kena päeva!");
+                }
+            }
+
+            Console.Clear();
+
+            foreach (string wish in myshoppingList)
+            {
+                Console.WriteLine($"Sinu nimekirjas: {wish}");
+            }
+
+            File.WriteAllLines($"{fileLocation}{fileNamed}", myshoppingList);
+
+        }
+    }
+}
